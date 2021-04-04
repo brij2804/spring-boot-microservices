941e84f40488fd2ca195bf28e6183cfecfefc361
brij2804 <brij2804@users.noreply.github.com>
2021-04-04 13:33:20 +0530

    API gateway implementation

---
+++ SpringbootMicroservicesPart5/netflix-zuul-api-gateway-server/src/main/java/com/brijesh/microservices/netflixzuulapigatewayserver/NetflixZuulApiGatewayServerApplication.java
@@ -0,0 +1,19 @@
+package com.brijesh.microservices.netflixzuulapigatewayserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.cloud.netflix.zuul.EnableZuulServer;
+
+
+@SpringBootApplication
+@EnableZuulProxy
+@EnableDiscoveryClient
+public class NetflixZuulApiGatewayServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NetflixZuulApiGatewayServerApplication.class, args);
+    }
+
+}
